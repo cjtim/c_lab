b40bfc2cd9e770a8b802614ab76db2747a9f6a6f
cjtim <47479090+cjtim@users.noreply.github.com>
2019-12-27 17:59:40 +0700

    02-02

--- 02-02.c
+++ 02-02.c
@@ -8,24 +8,39 @@ int main(){
     fgets(money_char,10,stdin);
     float money = atof(money_char);
     float summ;
+    int starleft;
     if(star == 1){
         //10%
-        float sum = money * (0.1);
+        summ = money * (0.9);
+        printf("You get 10 percents discount.\n");
+        starleft = star - 1;
     }
     else if(star == 2){
-        float sum = money * (0.15);
+        summ = money * (0.85);
+        printf("You get 15 percents discount.\n");
         //20%
+        starleft = star - 2;
     }
     else if (star < 6){
-        float sum = money * (0.2);
+        summ = money * (0.8);
+        printf("You get 20 percents discount.\n");
         //30%
+        starleft = star - 3;
     }
     else if (star < 9 ){
-        float sum = money * (0.3);
+        summ = money * (0.7);
+        printf("You get 30 percents discount.\n");
         //40%
+        starleft = star - 6;
     }
     else if (star >=9  ){
-        float sum = money * (0.4);
+        summ = money * (0.6);
+        printf("You get 40 percents discount.\n");
+        starleft = star - 9;
     }
-    printf("%f",summ);
+    
+    // printf("star is %d\n",star);
+    // printf("money is %f\n",money);
+    printf("Total amount due is %.2f Baht.\n",summ);
+    printf("And you have %d stickers left.",starleft);
 }
