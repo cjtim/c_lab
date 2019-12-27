e33ddbca4e607723c3d46f35c05a02cd5d51cc63
cjtim <47479090+cjtim@users.noreply.github.com>
2019-12-27 21:37:12 +0700

    master

--- 02-05.c
+++ 02-05.c
@@ -2,11 +2,29 @@
 #include <stdlib.h>
 int main(){
      printf("Enter length of side A: ");
-     int a = getchar();
-     printf("%d",a);
+     char a_str[3],b_str[3],c_str[3];
+     gets(a_str);
+     
+     // printf("%d\n",a);
+     printf("Enter length of side B: ");
+     gets(b_str);
+     printf("Enter length of side C: ");
+     gets(c_str);
 
-
-
-
-
+     int a = atoi(a_str);
+     int b = atoi(b_str);
+     int c = atoi(c_str);
+     if ((a <= 0 | b <= 0 | c <= 0) | (a+b <= c | b+c <= a | a+c <= b)){
+          printf("Triangle type is invalid.");
+     }
+     else if (a==b & b == c & a == c){ //สามเหลี่ยมด้านเท่า (equilateral) ทั้งสามด้านเท่ากัน
+          printf("Triangle type is equilateral.\n");
+     }
+     else if (a == b | b == c | a == c )
+     {
+          printf("Triangle type is isosceles.\n");
+     }
+     else if (a != b & b != c & a != c){
+          printf("Triangle type is scalene.\n");
+     }
 }
