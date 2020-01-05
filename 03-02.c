a2cfff857d4038baf60244fff756257640458caf
cjtim <47479090+cjtim@users.noreply.github.com>
2020-01-05 15:54:30 +0700

    PASS 03-02.c and 03-03.c

--- 03-02.c
+++ 03-02.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    char m_str[10],n_str[10000];
-    gets(m_str);
-    gets(n_str);
+    char m_str[20],n_str[20];
+    fgets(m_str,20,stdin);
+    fgets(n_str,20,stdin);
     long long m = atoll(m_str);
     long long n = atoll(n_str);
     int i,gcd,a,b,c;
     long long int lcm;
+    if (m > n){
+        b = m;
+        a = n;
+    }
+    else{
     a = m;
     b = n;
+    }
     
     //Calculate GCD
     c = a % b;
@@ -22,7 +28,6 @@ int main(){
     gcd = b;
     // END OF GCD
     lcm = (m * n) / gcd;
-    printf("GCD: %d\n", gcd);
+    printf("GCD: %lld\n", gcd);
     printf("LCM: %lld\n", lcm);
-
 }
--- 03-03.c
+++ 03-03.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    int i  , max, a, b;
+    int i  , max, a, b, c, first, sec, gcd;
     char a_str[20],b_str[20];
     fgets(a_str,20,stdin);
     fgets(b_str,20,stdin);
-    a = atoi(a_str);
-    b = atoi(b_str);
-    
+    first = atoi(a_str);
+    sec = atoi(b_str);
+    //printf("first = %d\nsec = %d  before\n",first,sec);
+    a = first;
+    b = sec;
+    //Calculate GCD
+    c = a % b;
+    while(c > 0)
+    {
+        a = b;
+        b = c;
+        c = a % b;
+    }
+    gcd = b;
+    // END OF GCD
+    first = first / gcd;
+    sec = sec / gcd;
+    //printf("first = %d\nsec = %d\n",first,sec);
+    if (sec==1){
+    printf("= %d\n",first);
+    }
+    else{
+        printf("= %d/%d\n",first,sec);
+    }
 }
