56c3458ca8876fc0bb38ac906b8c67e1fbff3a16
cjtim <47479090+cjtim@users.noreply.github.com>
2020-01-09 23:06:49 +0700

    04-02.c uncomplete

---
+++ 04-02.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+int is_prime(int x){
+    int count=0;
+    for (int j = 2;j<=x;j++){
+        if (x % j == 0){
+            count += 1;
+        }
+        if (count == 0){
+            return 0;
+        }
+        else if (count == 2)
+        {
+            return 1;
+        }
+    }
+}
+
+int main() {
+  char input[5];
+  fgets(input, 5, stdin);
+
+  int i, n;
+
+  n = atoi(input);
+
+  for (i = 0; i < n; i++) {
+      if (is_prime(i)) {
+          printf("%d is a prime number.\n", i);
+      }
+  }
+
+  return 0;
+}
