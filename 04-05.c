48569553242fa79c822d632213154dab23d42138
cjtim <47479090+cjtim@users.noreply.github.com>
2020-01-12 19:51:55 +0700

    Update 04-05.c

--- 04-05.c
+++ 04-05.c
@@ -8,8 +8,30 @@ int main(){
         printf("-");
     }
     else{
-        for (int i = 1; i < (2*num-1); i++){
+        //i แถว
+        for (int i = 1; i <= (2*num-1); i++){
+            // j หลัก
+            int code = 96+num;
+            int code1 = 96;
+            for (int j = 1; j <= ((4*num-2)); j++){
+                if (j >= (2*num-1)){
+                    printf("%c",code1);
+                    code1++;
+                }
+                else if (j < (2*num-1)){
+                    printf("%c",code);
+                    code--;
+                }
+                if(j != 1 || j != 4*num-2){
+                    printf("-");
+                }
+                //printf("j is %d\n",j);
+                
+            }
+            printf("\n");
 
         }
     }
 }
+//------------------j------------------ 19 37 2*num-1 4*num-3
+//j-i-h-g-f-e-d-c-b-a-`-_-^-]-\-[-Z-Y-X-
