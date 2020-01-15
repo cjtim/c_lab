1aaf245e86dff148e4222ba1a9349d81f2081207
cjtim <47479090+cjtim@users.noreply.github.com>
2020-01-15 21:26:07 +0700

    Update 04-08.c

--- 04-08.c
+++ 04-08.c
@@ -1,8 +1,18 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
-int binary(int x){
-    
-}
-int main(){
-
-}
+int find(int decimal_number) 
+{ 
+    if (decimal_number <= 0)  
+        return 0;  
+    else
+        return (decimal_number % 2 + (10 * find(decimal_number / 2))); 
+} 
+int main() 
+{ 
+    char num_str[20];
+    fgets(num_str, 20, stdin);
+    int decimal_number = atoi(num_str);
+    printf("%d", find(decimal_number)); 
+    printf("\n");
+    return 0; 
+} 
