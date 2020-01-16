b13585bf9bea125a3323cc8b10afc73c76ba1212
cjtim <47479090+cjtim@users.noreply.github.com>
2020-01-16 15:55:48 +0700

    Update 04-08.c

--- 04-08.c
+++ 04-08.c
@@ -1,18 +1,22 @@
 #include <stdio.h> 
 #include <stdlib.h>
-int find(int decimal_number) 
+// Decimal to binary conversion 
+// using recursion 
+
+long int find(int decimal_number) 
 { 
     if (decimal_number <= 0)  
         return 0;  
     else
         return (decimal_number % 2 + (10 * find(decimal_number / 2))); 
 } 
+// Driver code  
 int main() 
 { 
     char num_str[20];
     fgets(num_str, 20, stdin);
-    int decimal_number = atoi(num_str);
-    printf("%d", find(decimal_number)); 
+    long int decimal_number = atol(num_str);
+    printf("%ld", find(decimal_number)); 
     printf("\n");
     return 0; 
 } 
