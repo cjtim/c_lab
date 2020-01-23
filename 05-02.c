5b85ffe142de9f9414a5fe0ead9de14a5f4efaf7
cjtim <47479090+cjtim@users.noreply.github.com>
2020-01-23 21:19:11 +0700

    Update 05-02.c

--- 05-02.c
+++ 05-02.c
@@ -1,29 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-int i = 0 , j = 0, k = 0;
+
+
 int triple(int num){
-    for (i = 0; i < num; i++){
-        for(j = 0; j < num/2; j++){
-            for(k = 0; k < num/2; k++){
-                //printf("i = %d, j = %d, k= %d\n",i,j,k);
-                if (pow(i,2)+pow(j,2) == pow(k,2) && 
-                    i != 0 && j != 0 && k != 0 && i+j+k == num){
-                    //printf("******** i = %d, j = %d, k= %d\n",i,j,k);
-                    return 1;
+    int a = 0, b = 0, c = 0;
+    int m = 2, count = 0;
+    while(a + b + c <= num){
+        for(int n = 1; n < m; n++){
+            a = m*m - n*n;
+            b = 2 * m * n;
+            c = m*m + n*n;
+
+            
+            if (a + b + c == num){
+                if ( a > b && a > c){
+                    int temp = a;
+                    a = b;
+                    b = c;
+                    c = temp;
                 }
+                if (a>b){
+                    int temp = a;
+                    b = a;
+                    a = temp;
+                }
+                if (b > c){
+                    int temp = b;
+                    b = c;
+                    c = temp;
+                }
+                printf(" %d %d %d\n",a,b,c); 
+                count = 1;
             }
         }
-
+        m++;
     }
+    printf(" %d %d %d\n",a,b,c); 
 }
 int main(){
     int num;
     scanf("%d", &num);
     triple(num);
-    if (i == j && j == k && i == k){
-        printf("No triple found.\n");
-    }else{
-        printf("(%d, %d, %d)\n",i,j,k);
-    }
+    // pythagoreanTriplets(num);
+    //sort
+
+
+
 }
