e754450a185a7cfa08f03380eb8d3a3d40ce37e1
cjtim <47479090+cjtim@users.noreply.github.com>
2020-01-29 14:39:30 +0700

    cleanup folder

---
+++ Elab_C_File/02-02.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    char star_char[10],money_char[10];
+    fgets(star_char,10,stdin); // รับ
+    int star = atoi(star_char);
+    
+    fgets(money_char,10,stdin);
+    float money = atof(money_char);
+    float summ;
+    int starleft;
+    if(star == 1){
+        //10%
+        summ = money * (0.9);
+        printf("You get 10 percents discount.\n");
+        starleft = star - 1;
+    }
+    else if(star == 2){
+        summ = money * (0.85);
+        printf("You get 15 percents discount.\n");
+        //20%
+        starleft = star - 2;
+    }
+    else if (star < 6 & star > 0){
+        summ = money * (0.8);
+        printf("You get 20 percents discount.\n");
+        //30%
+        starleft = star - 3;
+    }
+    else if (star < 9 & star > 0){
+        summ = money * (0.7);
+        printf("You get 30 percents discount.\n");
+        //40%
+        starleft = star - 6;
+    }
+    else if (star >=9  & star > 0){
+        summ = money * (0.6);
+        printf("You get 40 percents discount.\n");
+        starleft = star - 9;
+    }
+    else {
+        summ = money ;
+        printf("You get 0 percents discount.\n");
+    }
+    
+    // printf("star is %d\n",star);
+    // printf("money is %f\n",money);
+    printf("Total amount due is %.2f Baht.\n",summ);
+    printf("And you have %d stickers left.\n",starleft);
+}
