02a4d3dc27c41afebeeebd15e35231e80300d944
cjtim <47479090+cjtim@users.noreply.github.com>
2020-01-30 22:09:28 +0700

    Update 06-03.c

--- Elab_C_File/06-03.c
+++ Elab_C_File/06-03.c
@@ -6,14 +6,15 @@ int main() {
 
   for (i = 2; i < ARRAY_SIZE; i++){
       A[i] = 1;
-}
-  for (int p = 0; p < ARRAY_SIZE; p++ ) {
-      if (p % A[p] != 0){
-          A[p] = 0;
-      }
+  }
+  
+  for (int i = 0; i < ARRAY_SIZE; i++ ) {
+    for (int j = 0; j < ){
+      if (A[i] == )
+    }
+
   }
   for (i = 2; i < ARRAY_SIZE; i++){
-
     printf("%d ", A[i]  );
   }
 
