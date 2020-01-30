569f51fc2b8d097a6d61af3ccc862368a52db598
cjtim <47479090+cjtim@users.noreply.github.com>
2020-01-30 15:06:36 +0700

    Update 06-04.c

--- Elab_C_File/06-04.c
+++ Elab_C_File/06-04.c
@@ -6,12 +6,12 @@ void setPieceOnTable(int board[][BOARD_SIZE], char piece, int xPos, int yPos);
 
 int main() {
     int left,xPos,yPos;
-    char piece;
+    int piece;
     int board[BOARD_SIZE][BOARD_SIZE];
     //set all in board to space(' ')
     for (int i = 0; i < BOARD_SIZE; i++){
         for(int j = 0; j < BOARD_SIZE; j++){
-            board[i][j] = ' ';
+            board[i][j] = 0;
         }
     }
     int storex[65];
@@ -19,49 +19,53 @@ int main() {
     int storepiece[65];
     scanf("%d",&left);
     for(int i = 0; i < left; i++){
-        scanf(" %c(%d, %d)",&piece,&xPos,&yPos);
+        scanf(" %lc(%d, %d)",&piece,&xPos,&yPos);
+        board[xPos][yPos] = piece;
         storex[i] = xPos;
         storey[i] = yPos;
         storepiece[i] = piece;
-        // setPieceOnTable( board, piece , xPos, yPos);
+        // printf("storepiece[%d] = %c\n", i, storepiece[i]);
     }
     printf("------------------\n");
     printf("  0 1 2 3 4 5 6 7\n");
     //send x and y to function
-    // for(int i = 0; i < left; i++){
-       
-    //     // setPieceOnTable( board, storepiece[i] , storex[i], storey[i]);
-    // }
-    int *yPtr = storey, *xPtr = storex;
-    int *piecePtr = storepiece;
-    for(int i = 0; i < BOARD_SIZE ; i++){
-        printf("%d|",i);
-        for(int j = 0; j < BOARD_SIZE; j++){
-            if (i == *yPtr && j == *xPtr){
-                printf("%c|",*piecePtr);
-                yPtr++;
-                xPtr++;
-                piecePtr++;
-            }
-            else{
+    int send = 0;
+    for (int y = 0; y < BOARD_SIZE; y++){
+        printf("%d|",y);
+        for (int x = 0; x < BOARD_SIZE; x++){
+            if(board[x][y] == 0){
                 printf(" |");
             }
+            else{
+                printf("%c|",board[x][y]);
+            }
+            
         }
         puts("");
+
     }
+
 }
-// void setPieceOnTable(int board[][BOARD_SIZE], char piece, int xPos, int yPos ){
-//     for(int i = 0; i < BOARD_SIZE ; i++){
-//          printf("%d|",i);
-//         for(int j = 0; j < BOARD_SIZE; j++){
-//             if (i == yPos && j == xPos){
-//                 printf("%c|",piece);
-//             }
-//             else{
-//                 printf(" |");
-//             }
-//         }
-//         puts("");
-//     }
-    
-// }
+void setPieceOnTable(int board[][BOARD_SIZE], char piece, int xPos, int yPos ){
+    printf("%c|",piece);
+}
+
+
+
+    // int *yPtr = storey, *xPtr = storex;
+    // int *piecePtr = storepiece;
+    // for(int i = 0; i < BOARD_SIZE ; i++){
+    //     printf("%d|",i);
+    //     for(int j = 0; j < BOARD_SIZE; j++){
+    //         if (i == *yPtr && j == *xPtr){
+    //             printf("%c|",*piecePtr);
+    //             yPtr++;
+    //             xPtr++;
+    //             piecePtr++;
+    //         }
+    //         else{
+    //             printf(" |");
+    //         }
+    //     }
+    //     puts("");
+    // }
