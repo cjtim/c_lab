0f2a2f0600c675ea31589dd40fbb317997f81288
cjtim <47479090+cjtim@users.noreply.github.com>
2020-03-12 18:58:20 +0700

    Update 09-05.c

--- Elab_C_File/09-05.c
+++ Elab_C_File/09-05.c
@@ -1,19 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedey struct Node { 
+typedef struct Node { 
    int data; 
    struct node *next; 
 }node;
 
 
-void printList(node *pList) {
-
+void insertNode(node **head, int input[], int n)
+{  int i;
+   for (i=0; i < n; i++) {
+    if (*head == NULL){
+        *head = (node *)malloc(sizeof(node));
+    }
+    else {
+        (*head)->next = (node *)malloc(sizeof(node));
+        *head = (*head)->next;
+    }
+    (*head)->next = NULL;
+   }
 }
-
- insertNode(
-) {
-
+/* function to swap data of two nodes a and b*/
+void swap(struct node *a, struct node *b) 
+{ 
+    int temp = a->data; 
+    a->data = b->data; 
+    b->data = temp; 
+} 
+/* Bubble sort the given linked list */
+void bubbleSort(struct node *head) 
+{ 
+    // PRE_NODE | NODE 
+    int swapped, i; 
+    struct node *NODE; 
+    struct node *LAST_NODE = NULL; 
+    /* Checking for empty list */
+    if (head == NULL) 
+        return; 
+    do
+    { 
+        swapped = 0; 
+        NODE = head; 
+        while (NODE->next != LAST_NODE) 
+        { 
+            if (NODE->data > (NODE->next)->data) 
+            {  
+                swap(NODE, NODE->next); 
+                swapped = 1; 
+            } 
+            NODE = NODE->next; 
+        } 
+        LAST_NODE = NODE; 
+    }
+    while (swapped);
 }
 
 int main() {
