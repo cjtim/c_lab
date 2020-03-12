b676979423d2c20ce6ccfd508cd7c9d1cfac7c62
cjtim <47479090+cjtim@users.noreply.github.com>
2020-03-12 23:09:04 +0700

    Update 09_5-04.cpp

--- Elab_C_File/09_5-04.cpp
+++ Elab_C_File/09_5-04.cpp
@@ -1,57 +1,55 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-struct Word_ {
+
+typedef struct Word_ {
     string word;
     int count;
-
-};
+} Word;
 
 int main() {
     string input;
     int i, last_word = 0, find = 0;
-    struct Word_ data[20];
+    Word data[20];
 
     while (true) {
         cin >> input;
         if (input == "exit") {
             break;
         }
-            i = 0;
-            int break_loop = 0;
-            data[0].word = input;
-            data[0].count += 1;
-            i++;
-            last_word++;
-            // printf("    last_word is %d\n", last_word);
-            
-            while(1){
-                cin >> input;
-                if(input == "exit"){
-                    break;
-                }
-                else{
-                    for(int j = 0; j < i; j++){
-                        if(data[j].word == input){
-                            data[j].count += 1;
-                            break_loop = 1;
-                            break;
-                        }
-                    }
-                    if(break_loop == 0){
-                        data[i].word = input;
-                        data[i].count += 1;
-                        last_word++;
-                        i++;
-                    }
-                }
-                break_loop = 0;
+        i = 0;
+        int break_loop = 0;
+        data[0].word = input;
+        data[0].count += 1;
+        i++;
+        last_word++;
+        while(1){
+            cin >> input;
+            if(input == "exit"){
+                break;
             }
-            break;
+            else{
+                for(int j = 0; j < i; j++){
+                    if(data[j].word == input){
+                        data[j].count += 1;
+                        break_loop = 1;
+                        break;
+                    }
+                }
+                if(break_loop == 0){
+                    data[i].word = input;
+                    data[i].count += 1;
+                    last_word++;
+                    i++;
+                }
+            }
+            break_loop = 0;
         }
-
-        cout << "Output:" << endl;
-        for(i=0;i<last_word;i++){
-                cout << data[i].word << " = " << data[i].count << endl;;
-                // printf("data[%d].word = %s data[%d].count = %d\n", i, data[i].word, i, data[i].count);
-        }
+        break;
+    }
+    cout << "Output:" << endl;
+    for(i=0;i<last_word;i++){
+                    cout << data[i].word << " = " << data[i].count << endl;;
+                    // printf("data[%d].word = %s data[%d].count = %d\n", i, data[i].word, i, data[i].count);
+            }
+    return 0;
 }
