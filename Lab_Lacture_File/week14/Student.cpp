be49ed40de344a5531205ddc85bacec9e7601757
cjtim <47479090+cjtim@users.noreply.github.com>
2020-03-20 14:58:13 +0700

    Update Student.cpp

--- Lab_Lacture_File/week14/Student.cpp
+++ Lab_Lacture_File/week14/Student.cpp
@@ -1,5 +1,29 @@
 #include<iostream>
+#include <sstream>
 #include "Person.h"
 #include "Student.h"
 using namespace std;
 
+Student::Student(string firstname, string lastname, string id)
+    : Person (firstname, lastname, id){
+        score = 0;
+}
+
+void Student::addScore(double score){
+    if (score > 0){
+        this->score = score;
+    }
+}
+
+string Student::calculateGrade(){
+    if(score > 80) return "A";
+    if (score > 70) return "B+";
+    return "F";
+}
+double Student::getScore(){
+    return score;
+}
+string Student::toString(){
+    stringstream ss;
+    ss << Person::tostring
+}
