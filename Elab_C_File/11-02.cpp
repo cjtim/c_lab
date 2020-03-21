d3f2acd9511b24c55b8008dabd296174326a0b4f
cjtim <47479090+cjtim@users.noreply.github.com>
2020-03-21 12:23:42 +0700

    PASSED

--- Elab_C_File/11-02.cpp
+++ Elab_C_File/11-02.cpp
@@ -22,18 +22,18 @@ public:
 };
 
 Animal::Animal(int happiness, int energy, int fullness){
-    this->happiness = happiness;
-    this->energy = energy;
-    this->fullness = fullness;
+    this->happiness = (happiness);
+    this->energy = (energy);
+    this->fullness = (fullness);
     limitStat();
 }
 void Animal::limitStat(){
     if(happiness < 0){happiness = 0;}
-    if(happiness > 100){happiness = MAX_FULLNESS;}
+    else if(happiness > 100){happiness = MAX_FULLNESS;}
     if(energy < 0){energy = 0;}
-    if(energy > 100){energy = MAX_FULLNESS;}
+    else if(energy > 100){energy = MAX_FULLNESS;}
     if (fullness < 0){fullness = 0;}
-    if (fullness > 100){fullness = MAX_FULLNESS;}
+    else if (fullness > 100){fullness = MAX_FULLNESS;}
 }
 int Animal::getHappiness(){
     return happiness;
@@ -45,16 +45,18 @@ int Animal::getEnergy(){
     return energy;
 }
 void Animal::eat(int food){
-    if(fullness + food > 100){
-        fullness += food;
+    if (food < 0) food = 0;
+    else if(fullness + (food) > 100){
+        fullness += (food);
         happiness -= 10;
     }
     else{
-        fullness += food;
+        fullness += (food);
     }
     limitStat();
 }
 void Animal::play(int hour){
+    if (hour < 0) hour = 0;
     if (energy > 0 && fullness > 0 ){
         energy -= (10 * hour);
         fullness -= (20 * hour);
@@ -63,6 +65,8 @@ void Animal::play(int hour){
     limitStat();
 }
 void Animal::sleep(int hour){
+    if (hour < 0) hour = 0;
+    hour = abs(hour);
     energy += (10 * hour);
     fullness -= (10 * hour);
     limitStat();
@@ -72,19 +76,20 @@ void Animal::sleep(int hour){
 
 int main()
 {
-    Animal a1(100, 100, 100);
-    a1.eat(0);
+    Animal a1(1000, 1000, 1000);
+    a1.play(-5);
     cout << "Energy : " << a1.getEnergy() << endl;
     cout << "Fullness : " << a1.getFullness() << endl;
     cout << "Happyness : " << a1.getHappiness() << endl;
 
-    a1.play(0);
+    
+    a1.eat(70);
     cout << endl;
     cout << "Energy : " << a1.getEnergy() << endl;
     cout << "Fullness : " << a1.getFullness() << endl;
     cout << "Happyness : " << a1.getHappiness() << endl;
 
-    a1.sleep(0);
+    a1.sleep(5);
     cout << endl;
     cout << endl;
     cout << "Energy : " << a1.getEnergy() << endl;
