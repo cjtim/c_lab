9e96097c1869abce3851852feb2dfb7d707f3841
cjtim <47479090+cjtim@users.noreply.github.com>
2020-03-27 21:59:38 +0700

    Create Test.cpp

---
+++ Elab_C_File/Test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+int num = 0;
+switch (expression) { 
+    case const-expr: statements 
+    case const-expr: statements 
+    default: statements 
+}
+if (expression) 
+    statement1 
+else 
+    statement2
+
+
+
+
+
